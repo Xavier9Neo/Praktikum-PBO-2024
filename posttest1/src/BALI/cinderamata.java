43eda9a5e698386d47673bf823ea11c8462c204b
Xavier9Neo <119903079+Xavier9Neo@users.noreply.github.com>
2024-03-11 12:16:19 +0700

    cinderamata.java

--- posttest1/src/BALI/cinderamata.java
+++ posttest1/src/BALI/cinderamata.java
@@ -9,24 +9,6 @@ public class cinderamata{
         this.jeniscdm = jeniscdm;
         this.hargacdm = hargacdm;
     }
-    // public String getjeniscdm() {
-    //     return jeniscdm;
-    // }
-    // public int gethargacdm() {
-    //     return hargacdm;
-    // }
-    // public String getnamacdm() {
-    //     return namacdm;
-    // }
-    // public void setjeniscdm(String jeniscdm) {
-    //     jeniscdm = Jeniscdm;
-    // }
-    // public void setnamacdm(String namacdm) {
-    //     namacdm = namacdm;
-    // }
-    // public void sethargacdm(int hargacdm) {
-    //     hargacdm = hargacdm;
-    // }
     void tampil(){
         System.out.println("Nama :" + this.namacdm);
         System.out.println("Jenis:" + this.jeniscdm);
